1b82856277544e8ec5bbf8e733a6856fdf6946e0
Gibson Han <66423127+gibsonliketheguitar@users.noreply.github.com>
2022-06-17 23:04:04 -0700

    add styling to dashboard component

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,10 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { useTheme } from "@emotion/react";
+import { Box, Paper, styled } from "@mui/material";
+import { Container } from "@mui/system";
+
+import Roster from "./Roster";
+import Formation from "./Formation";
+
+const Icon = styled(Paper)(({ theme }) => ({
+  height: theme.spacing(4),
+  width: theme.spacing(4),
+  marginBottom: theme.spacing(3),
+  backgroundClip: theme.palette.secondary.main,
+}));
 
 export default function Dashboard() {
+  const theme: any = useTheme();
+  const [page, setPage] = useState(0);
+
+  const DISPLAY: any = {
+    0: <Roster />,
+    1: <Formation />,
+  };
+
   return (
-    <div>
-      <div>sidebar</div>
-      <div>main</div>
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        height: "100vh",
+        width: "100%",
+        backgroundColor: "#111111",
+      }}
+    >
+        <Box
+        sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          marginTop: theme.spacing(4),
+            marginRight: theme.spacing(2),
+            marginLeft: theme.spacing(2),
+          }}
+        >
+        <Icon sx={{ marginBottom: theme.spacing(6) }}>Logo</Icon>
+          <Icon onClick={() => setPage(0)}>Icon</Icon>
+          <Icon onClick={() => setPage(1)}>Icon</Icon>
+        </Box>
+        <Container
+          sx={{
+            padding: theme.spacing(4),
+          backgroundColor: theme.palette.background.default,
+          }}
+        >
+          {DISPLAY[page]}
+        </Container>
+    </Box>
   );
 }
